d13677772330602b2aae4df82ab775a640f56ab9
James Hammer <branson2015@gmail.com>
2021-11-21 02:25:55 -0500

    added cli.py

---
+++ cli.py
@@ -0,0 +1,35 @@
+def docli(cli):
+    if cli == '':
+        return []
+
+    args = cli.split()
+
+    updates = []
+    if args[0] == 'add':
+        idx = 0
+        if len(args) != 3:
+            print('useage: add idx atom')
+            return
+        idx = int(args[1])
+        a = args[2]
+        if a not in ['+']:
+            a = int(a)
+        updates.append({'addAtom': [a, idx]})
+    elif args[0] == 'convert':
+        if len(args) != 3:
+            print('usage: convert idx atom')
+            return
+        idx = int(args[1])
+        a = args[2]
+        if a not in ['+']:
+            a = int(a)
+        updates.append({'convertTo': [idx, a]})
+    elif args[0] == 'delete':
+        if len(args) != 2:
+            print('usage: delete idx')
+        idx = int(args[1])
+        updates.append({'delete': [idx]})
+    elif args[0] == 'exit':
+        raise Exception()
+        
+    return updates
